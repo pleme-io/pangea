f02e84e1b6977546cfe79671c214fda08e6dd09a
luis <ldesiqueira@pinger.com>
2023-07-07 21:48:39 -0300

    init

--- lib/pangea/cli/subcommands/config.rb
+++ lib/pangea/cli/subcommands/config.rb
@@ -7,8 +7,32 @@ class ConfigCommand < PangeaCommand
     command %(config)
   end
 
+  argument :subcommand do
+    desc %(subcommand for config)
+    required
+  end
+
+  def help
+    <<~HELP
+    Usage: pangea config [OPTIONS] SUBCOMMAND
+
+    Arguments:
+      SUBCOMMAND  subcommand for config
+
+    Options:
+      -h, --help    Print usage
+    HELP
+  end
+
   def run(argv)
     parse(argv)
+    puts argv
     print help
+
+    case params[:subcommand].to_s
+    when %(show)
+      nil
+    end
+
   end
 end
