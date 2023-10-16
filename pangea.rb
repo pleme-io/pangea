f7cb1db886f0522331c99a3823ca2fe9fe663b2d
luis <ldesiqueira@pinger.com>
2023-10-16 11:37:00 -0300

    formatting

--- pangea.rb
+++ pangea.rb
@@ -2,17 +2,19 @@ namespace :nexus, :atavan do
   long  :atavan
   short :at
 
-  state_config({
-    terraform: {
-      s3: {
-        bucket: %(atavan-terraform-state),
-        key: %(atavan/terraform.tfstate),
-        region: %(us-west-2),
-        dynamodb_table: %(atavan-terraform-lock),
-        encrypt: true
+  state_config(
+    {
+      terraform: {
+        s3: {
+          bucket: %(atavan-terraform-state),
+          key: %(atavan/terraform.tfstate),
+          region: %(us-west-2),
+          dynamodb_table: %(atavan-terraform-lock),
+          encrypt: true
+        }
       }
     }
-  })
+  )
 
   # environments(
   #   {
