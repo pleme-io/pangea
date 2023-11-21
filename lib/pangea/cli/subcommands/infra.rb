e443fe6a9440579a55e7ccb163621da76e344765
luis <luisfelipemourapereira@protonmail.com>
2023-11-21 08:04:01 +0100

    almost done with module evaluation

--- lib/pangea/cli/subcommands/infra.rb
+++ lib/pangea/cli/subcommands/infra.rb
@@ -8,8 +8,8 @@ require %(pangea/errors/no_infra_target_error)
 require %(pangea/errors/incorrect_subcommand_error)
 require %(pangea/say/init)
 require %(pangea/modules)
-require %(terraform-synthesizer)
-require %(json)
+# require %(terraform-synthesizer)
+# require %(json)
 
 class InfraCommand < PangeaCommand
   include Constants
@@ -29,20 +29,74 @@ class InfraCommand < PangeaCommand
     desc %(target like ${namespace}.${site})
   end
 
-  def run(argv)
-    Say.terminal %(planning!)
-    parse(argv)
-
-    # grab a config synth
-    cfg_synth = Config.resolve_configurations
-
-    # reject empty configurations
+  def reject_empty_configurations
     if cfg_synth.empty?
       Say.terminal %(configuration empty, exiting...)
       exit
     end
+  end
 
+  def help
+    <<~HELP
+      Usage: pangea infra command [OPTIONS] SUBCOMMAND
+
+      Arguments:
+        SUBCOMMAND  subcommand for pangea infra
+
+      Subcommands:
+        plan    plan infrastructure
+        apply   apply infrastructure
+        config  show run configuration
+    HELP
+  end
+
+  def cfg_synth
+    @cfg_synth ||= Config.resolve_configurations
+  end
+
+  def plan(argv)
+    Say.terminal %(planning!)
+    parse(argv)
+    process_modules
+  end
+
+  def config
     Say.terminal JSON.pretty_generate(cfg_synth)
+  end
+
+  def common_run_checks
+    reject_empty_configurations
+  end
+
+  def process_modules
+    namespaces = cfg_synth[:namespace].keys.map(&:to_sym)
+    namespaces.each do |namespace_name|
+      namespace     = cfg_synth[:namespace][namespace_name]
+      context_names = namespace.keys.map(&:to_sym)
+
+      context_names.each do |cn|
+        context = namespace[cn]
+        modules = context[:modules]
+
+        modules.each_key do |mod_key|
+          mod = modules[mod_key]
+          PangeaModule.process(mod)
+        end
+      end
+    end
+  end
+
+  def run(argv)
+    common_run_checks
+
+    if argv.length < 2
+      puts help
+      exit
+    end
+
+    config if argv[1].to_s.eql?(%(config))
+    plan(argv) if argv[1].to_s.eql?(%(plan))
+    apply(argv) if argv[1].to_s.eql?(%(apply))
 
     # preflight checks for the command execution
     # check_run
@@ -57,176 +111,174 @@ class InfraCommand < PangeaCommand
     # modules can have a virtual environment to execute in
     ###########################################################################
 
-    namespaces = cfg_synth[:namespace].keys.map(&:to_sym)
+    # namespaces = cfg_synth[:namespace].keys.map(&:to_sym)
 
-    namespaces.each do |namespace_name|
-      namespace     = cfg_synth[:namespace][namespace_name]
-      context_names = namespace.keys.map(&:to_sym)
-
-      context_names.each do |cn|
-        context = namespace[cn]
-        modules = context[:modules]
-
-        modules.each do |mod|
-          PangeaModule.process(mod)
-        end
-      end
-    end
+    # namespaces.each do |namespace_name|
+    #   namespace     = cfg_synth[:namespace][namespace_name]
+    #   context_names = namespace.keys.map(&:to_sym)
+    #
+    #   context_names.each do |cn|
+    #     context = namespace[cn]
+    #     modules = context[:modules]
+    #
+    #     modules.each do |mod|
+    #       PangeaModule.process(mod)
+    #     end
+    #   end
+    # end
 
     ###########################################################################
 
     # provide some kind of default exit of the command execution
-    exit
+    # exit
   end
-
-  private
 
   # process stage for target
-  def process_target(targets, cfg_synth)
-    namespaces    = cfg_synth[:namespace].keys.map(&:to_sym)
-    environments  = []
+  # def process_target(targets, cfg_synth)
+  #   namespaces    = cfg_synth[:namespace].keys.map(&:to_sym)
+  #   environments  = []
+  #
+  #   namespaces.each do |ns_name|
+  #     environments.concat(cfg_synth[:namespace][ns_name].keys.map(&:to_sym))
+  #   end
+  #
+  #   case targets.length.to_i
+  #     # only provided namespace
+  #   when 1
+  #     nil
+  #     # only provided namespace.site
+  #   when 2
+  #     nil
+  #     # only provided namespace.site.project
+  #   when 3
+  #     announce_preflight_info(targets, cfg_synth)
+  #     environments.each do |e_name|
+  #       projects = cfg_synth[:namespace][targets[0]][e_name][:projects]
+  #       projects.each do |project|
+  #         announce_project(project)
+  #
+  #         synth = TerraformSynthesizer.new
+  #         case project[:src][:type].to_sym
+  #         when :local
+  #           system %(mkdir -p #{CACHE_DIR}) unless Dir.exist?(CACHE_DIR)
+  #           PROJECT_SRC_DIRS.each do |src_dir|
+  #             next unless File.exist?(File.join(
+  #                                       project[:src][:location].to_s,
+  #                                       %(src),
+  #                                       src_dir.to_s
+  #                                     ))
+  #
+  #             synth_files = Dir.glob("#{File.join(
+  #               project[:src][:location].to_s,
+  #               %(src),
+  #               src_dir.to_s
+  #             )}/**/*.rb")
+  #             synth_files.each do |synth_file|
+  #               synth.synthesize(File.read(synth_file))
+  #             end
+  #           end
+  #
+  #           Say.terminal JSON.pretty_generate(synth.synthesis)
+  #
+  #           project_cache_dir = File.join(CACHE_DIR, project[:name].to_s)
+  #
+  #           system %(mkdir -p #{project_cache_dir}) unless Dir.exist?(project_cache_dir)
+  #           File.write(File.join(project_cache_dir, ARTIFACT_FILE), synth.synthesis.to_json)
+  #           system %(cd #{project_cache_dir} && terraform init)
+  #           system %(cd #{project_cache_dir} && terraform plan)
+  #
+  #         when :git
+  #           nil
+  #         end
+  #       end
+  #     end
+  #
+  #     # fetch project data
+  #     # projet_data = cfg_synth[:namespace][targets[0]]
+  #   end
+  # end
 
-    namespaces.each do |ns_name|
-      environments.concat(cfg_synth[:namespace][ns_name].keys.map(&:to_sym))
-    end
+  # def announce_project(project)
+  #   msg = []
+  #   msg << %(project: #{project[:name]})
+  #   msg << %(src:     #{project[:src]})
+  #   Say.terminal msg.map(&:strip).join(%(\n))
+  # end
 
-    case targets.length.to_i
-      # only provided namespace
-    when 1
-      nil
-      # only provided namespace.site
-    when 2
-      nil
-      # only provided namespace.site.project
-    when 3
-      announce_preflight_info(targets, cfg_synth)
-      environments.each do |e_name|
-        projects = cfg_synth[:namespace][targets[0]][e_name][:projects]
-        projects.each do |project|
-          announce_project(project)
-
-          synth = TerraformSynthesizer.new
-          case project[:src][:type].to_sym
-          when :local
-            system %(mkdir -p #{CACHE_DIR}) unless Dir.exist?(CACHE_DIR)
-            PROJECT_SRC_DIRS.each do |src_dir|
-              next unless File.exist?(File.join(
-                                        project[:src][:location].to_s,
-                                        %(src),
-                                        src_dir.to_s
-                                      ))
-
-              synth_files = Dir.glob("#{File.join(
-                project[:src][:location].to_s,
-                %(src),
-                src_dir.to_s
-              )}/**/*.rb")
-              synth_files.each do |synth_file|
-                synth.synthesize(File.read(synth_file))
-              end
-            end
-
-            Say.terminal JSON.pretty_generate(synth.synthesis)
-
-            project_cache_dir = File.join(CACHE_DIR, project[:name].to_s)
-
-            system %(mkdir -p #{project_cache_dir}) unless Dir.exist?(project_cache_dir)
-            File.write(File.join(project_cache_dir, ARTIFACT_FILE), synth.synthesis.to_json)
-            system %(cd #{project_cache_dir} && terraform init)
-            system %(cd #{project_cache_dir} && terraform plan)
-
-          when :git
-            nil
-          end
-        end
-      end
-
-      # fetch project data
-      # projet_data = cfg_synth[:namespace][targets[0]]
-    end
-  end
-
-  def announce_project(project)
-    msg = []
-    msg << %(project: #{project[:name]})
-    msg << %(src:     #{project[:src]})
-    Say.terminal msg.map(&:strip).join(%(\n))
-  end
-
-  def announce_preflight_info(targets, cfg_synth)
-    namespaces    = cfg_synth[:namespace].keys.map(&:to_sym)
-    environments  = []
-
-    namespaces.each do |ns_name|
-      environments.concat(cfg_synth[:namespace][ns_name].keys.map(&:to_sym))
-    end
-
-    preflight_info = []
-    preflight_info << %(environments: #{environments})
-    preflight_info << %(\n)
-    preflight_info << %(namespace: #{targets[0]})
-    preflight_info << %(site:      #{targets[1]})
-    preflight_info << %(project:   #{targets[2]})
-
-    Say.terminal preflight_info.map(&:strip).join(%(\n))
-  end
+  # def announce_preflight_info(targets, cfg_synth)
+  #   namespaces    = cfg_synth[:namespace].keys.map(&:to_sym)
+  #   environments  = []
+  #
+  #   namespaces.each do |ns_name|
+  #     environments.concat(cfg_synth[:namespace][ns_name].keys.map(&:to_sym))
+  #   end
+  #
+  #   preflight_info = []
+  #   preflight_info << %(environments: #{environments})
+  #   preflight_info << %(\n)
+  #   preflight_info << %(namespace: #{targets[0]})
+  #   preflight_info << %(site:      #{targets[1]})
+  #   preflight_info << %(project:   #{targets[2]})
+  #
+  #   Say.terminal preflight_info.map(&:strip).join(%(\n))
+  # end
 
   # targets can be...
   # ${namespace}
   # ${namespace}.${site}
-  def check_target(target, config)
-    # raise NamespaceNotFoundError if target.nil?
+  # def check_target(target, config)
+  #   # raise NamespaceNotFoundError if target.nil?
+  #
+  #   # targets       = target.split('.').map(&:to_sym)
+  #   # namespaces    = config[:namespace].keys.map(&:to_sym)
+  #   # runtype       = nil
+  #   # environments  = []
+  #
+  #   # namespaces.each do |ns_name|
+  #   #   environments.concat(config[:namespace][ns_name].keys.map(&:to_sym))
+  #   # end
+  #
+  #   # raise NamespaceNotFoundError unless namespaces.include?(targets[0])
+  #
+  #   # namespaces.each do |ns_name|
+  #   #   environments.each do |e_name|
+  #   #     sites = config[:namespace][ns_name][e_name][:sites] || []
+  #   #
+  #   #     next if sites.empty?
+  #   #
+  #   #     site_names = []
+  #   #     sites.each do |site|
+  #   #       site_names << site[:name]
+  #   #     end
+  #   #
+  #   #     raise SiteNotFoundError unless site_names.include?(targets[1].to_sym)
+  #   #
+  #   #     projects = config[:namespace][ns_name][e_name][:projects] || []
+  #   #
+  #   #     next if projects.empty?
+  #   #
+  #   #     project_names = []
+  #   #     projects.each do |project|
+  #   #       project_names << project[:name]
+  #   #     end
+  #   #
+  #   #     raise ProjectNotFoundError \
+  #   #     unless project_names
+  #   #            .include?(
+  #   #              targets[2].to_sym
+  #   #            )
+  #   #   end
+  #   # end
+  # end
 
-    # targets       = target.split('.').map(&:to_sym)
-    # namespaces    = config[:namespace].keys.map(&:to_sym)
-    # runtype       = nil
-    # environments  = []
+  # def check_run
+  #   raise IncorrectSubcommandError unless correct_subcommand?(
+  #     params[:subcommand]
+  #   )
+  #   raise NoInfraTargetError unless params[:target]
+  # end
 
-    # namespaces.each do |ns_name|
-    #   environments.concat(config[:namespace][ns_name].keys.map(&:to_sym))
-    # end
-
-    # raise NamespaceNotFoundError unless namespaces.include?(targets[0])
-
-    # namespaces.each do |ns_name|
-    #   environments.each do |e_name|
-    #     sites = config[:namespace][ns_name][e_name][:sites] || []
-    #
-    #     next if sites.empty?
-    #
-    #     site_names = []
-    #     sites.each do |site|
-    #       site_names << site[:name]
-    #     end
-    #
-    #     raise SiteNotFoundError unless site_names.include?(targets[1].to_sym)
-    #
-    #     projects = config[:namespace][ns_name][e_name][:projects] || []
-    #
-    #     next if projects.empty?
-    #
-    #     project_names = []
-    #     projects.each do |project|
-    #       project_names << project[:name]
-    #     end
-    #
-    #     raise ProjectNotFoundError \
-    #     unless project_names
-    #            .include?(
-    #              targets[2].to_sym
-    #            )
-    #   end
-    # end
-  end
-
-  def check_run
-    raise IncorrectSubcommandError unless correct_subcommand?(
-      params[:subcommand]
-    )
-    raise NoInfraTargetError unless params[:target]
-  end
-
-  def correct_subcommand?(sbcmd)
-    NAME.to_s.eql?(sbcmd.to_s)
-  end
+  # def correct_subcommand?(sbcmd)
+  #   NAME.to_s.eql?(sbcmd.to_s)
+  # end
 end
--- lib/pangea/modules.rb
+++ lib/pangea/modules.rb
@@ -5,6 +5,8 @@
 # for terraform code.
 ###############################################################################
 
+require %(terraform-synthesizer)
+
 module PangeaBase
   BASE_DIR = File.join(
     Dir.home,
@@ -28,12 +30,66 @@ module PangeaRbenv
   )
 
   class << self
+    def versions_dir
+      VERSIONS_DIR
+    end
+
     def rbenv_installed?
       `which rbenv`.strip != ''
     end
 
-    def rbenv_install(_version)
+    def rbenv_install(version, path)
       system %(mkdir -p #{VERSIONS_DIR}) unless Dir.exist?(VERSIONS_DIR)
+      if rbenv_installed? && !Dir.exist?(File.join(path.to_s))
+        puts [BIN, %(install), version.to_s, path.to_s].join(%( ))
+        system [BIN, %(install), version.to_s, path.to_s].join(%( ))
+      end
+    end
+  end
+end
+
+module PangeaRubyBuild
+  include PangeaBase
+  BIN = %(ruby-build).freeze
+  RUBY_BUILD_DIR = File.join(
+    BASE_DIR,
+    %(rbenv)
+  )
+  class << self
+    def ruby_build_installed?
+      `which rbenv`.strip != ''
+    end
+
+    def ruby_build(version, path)
+      system %(mkdir -p #{PangeaRbenv.versions_dir}) unless Dir.exist?(PangeaRbenv.versions_dir)
+      if ruby_build_installed? && !Dir.exist?(File.join(path.to_s))
+        puts [BIN, version.to_s, path.to_s].join(%( ))
+        system [BIN, version.to_s, path.to_s].join(%( ))
+      end
+    end
+
+    def gem_install(gem, ruby_version, gem_version, gemset_path)
+      gem_path = File.join(gemset_path, %(lib), %(ruby), %(gems), ruby_version.to_s, %(gems), %(#{gem}-#{gem_version}))
+      unless Dir.exist?(gem_path)
+        gembin = File.join(gemset_path, %(bin), %(gem))
+        puts [gembin, %(install), gem.to_s.strip, %(-v), gem_version.to_s].join(%( ))
+        system [gembin, %(install), gem.to_s.strip, %(-v), gem_version.to_s].join(%( ))
+      end
+    end
+
+    def bundle_install(mpath, gemset_path)
+      bundlebin = File.join(gemset_path, %(bin), %(bundle))
+      bundlehint = File.join(gemset_path, %(bundle_hint))
+      unless File.exist?(bundlehint)
+        cmd = [
+          # %(cd #{mpath} &&),
+          %(BUNDLE_GEMFILE=#{File.join(mpath, %(Gemfile))}), bundlebin,
+          %(install)
+        ].join(%( ))
+        puts cmd
+        system cmd
+        system [%(touch), bundlehint].join(%( ))
+      end
     end
   end
 end
@@ -51,15 +107,106 @@ module PangeaModule
     %(rubies)
   )
 
-  # SECTIONS = %i[
-  #   lib
-  #   src
-  # ].freeze
-
   class << self
+    def symbolize(hash)
+      JSON[JSON[hash], symbolize_names: true]
+    end
+
+    def terraform_synth
+      @terraform_synth ||= TerraformSynthesizer.new
+    end
+
+    SECTIONS = %i[
+      lib
+      src
+    ].freeze
+
     # entrypoint for module processing
     def process(mod)
-      exit 1 unless mod.fetch(:sandboxed, false)
+      mod = symbolize(mod)
+      if mod.fetch(:sandboxed)
+
+        ################################
+        # setup ruby environment for module
+        ################################
+
+        ruby_version    = mod.fetch(:ruby_version, %(3.1.0))
+        bundle_version  = mod.fetch(:ruby_version, %(2.3.3))
+        name            = mod.fetch(:name)
+
+        raise ArgumentError, %(name cannot be nil) if name.nil?
+
+        ruby_gemset = mod.fetch(:ruby_gemset, name)
+
+        gemset_path = File.join(
+          RUBIES_DIR,
+          ruby_version,
+          ruby_gemset
+        )
+
+        gems_load_path = File.join(
+          gemset_path,
+          %(gems)
+        )
+
+        PangeaRubyBuild.ruby_build(
+          ruby_version,
+          gemset_path
+        )
+
+        PangeaRubyBuild.gem_install(
+          %(bundler),
+          ruby_version,
+          bundle_version,
+          gemset_path
+        )
+
+        location  = mod.fetch(:location)
+        type      = location.fetch(:type)
+
+        if type.to_s.eql?(%(directory))
+          mpath = location.fetch(:path)
+          PangeaRubyBuild.bundle_install(
+            mpath,
+            gemset_path
+          )
+        end
+
+        # end setup ruby environment for module
+
+        ################################
+        # run module
+        ################################
+
+        # set the $LOAD_PATH to empty to make sure
+        # its clean
+        # $LOAD_PATH = []
+
+        # $LOAD_PATH.concat(gems_load_path)
+
+        # add ruby gems path to $LOAD_PATH
+        # $LOAD_PATH.unshift(File.join())
+
+        SECTIONS.each do |section|
+          rfiles = Dir.glob(
+            File.join(
+              mpath.to_s,
+              section.to_s,
+              %(**),
+              %(*.rb)
+            )
+          )
+          # rdirs = rfiles.map { |file_path| File.dirname(file_path) }
+          # rdirs = rdirs.uniq
+          rfiles.each do |rfile|
+            terraform_synth.synthesize(File.read(rfile))
+          end
+        end
+        puts terraform_synth.synthesis
+
+        # end run module
+
+      end
     end
   end
 end
