9a300addf742690cf942603cb1b63b2ce3c89e38
luis <ldesiqueira@pinger.com>
2025-04-15 23:22:46 -0300

    init

--- lib/pangea.rb
+++ lib/pangea.rb
@@ -1,7 +1,23 @@
+require 'pangea/config'
+require 'pangea/state'
+require 'pangea/utils'
+require 'pangea/cli'
+require 'json'
+
 module Pangea
-  class << self
-    def ping
-      %(pong)
+  autoload :Module, File.join(__dir__, 'pangea', 'module')
+
+  module App
+    class << self
+      def cfg
+        @cfg ||= Pangea::Utils.symbolize(
+          Pangea::Config.config
+        )
+      end
+
+      def run
+        Pangea::Cli.start(ARGV)
+      end
     end
   end
 end
