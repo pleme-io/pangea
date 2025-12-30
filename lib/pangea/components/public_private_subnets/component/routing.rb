c7c783075df906724136ee95e88c0b21b110521b
drzzln <drzzln@protonmail.com>
2025-12-29 20:17:28 -0500

    shinkaaaaaa

---
+++ lib/pangea/components/public_private_subnets/component/routing.rb
@@ -0,0 +1,216 @@
+# frozen_string_literal: true
+
+# Copyright 2025 The Pangea Authors. Licensed under Apache 2.0.
+
+module Pangea
+  module Components
+    module PublicPrivateSubnets
+      # Routing methods for NAT gateways and route tables
+      module Routing
+        def create_internet_gateway(name, component_attrs, vpc_id)
+          aws_internet_gateway(resource_name(name, :igw), {
+            vpc_id: vpc_id,
+            tags: merge_component_tags(
+              component_attrs.tags,
+              { Name: "#{name}-igw", Purpose: 'Public subnet internet access' },
+              :public_private_subnets,
+              :internet_gateway
+            )
+          })
+        end
+
+        def create_public_route_table(name, component_attrs, vpc_id, igw_ref)
+          public_rt_ref = aws_route_table(resource_name(name, :public_rt), {
+            vpc_id: vpc_id,
+            tags: merge_component_tags(
+              component_attrs.tags,
+              { Name: "#{name}-public-rt", Type: 'public', Purpose: 'Public subnet routing' },
+              :public_private_subnets,
+              :route_table
+            )
+          })
+
+          public_route_ref = aws_route(resource_name(name, :public_route), {
+            route_table_id: public_rt_ref.id,
+            destination_cidr_block: '0.0.0.0/0',
+            gateway_id: igw_ref.id
+          })
+
+          { route_table: public_rt_ref, route: public_route_ref }
+        end
+
+        def create_public_route_associations(name, public_subnets, public_rt_ref)
+          associations = {}
+          public_subnets.each do |subnet_key, subnet_ref|
+            assoc_name = resource_name(name, "#{subnet_key}_rt_assoc")
+            assoc_ref = aws_route_table_association(assoc_name, {
+              subnet_id: subnet_ref.id,
+              route_table_id: public_rt_ref.id
+            })
+            associations[:"#{subnet_key}_association"] = assoc_ref
+          end
+          associations
+        end
+
+        def create_nat_gateways(name, component_attrs, public_subnets, azs)
+          return { nat_gateways: {}, nat_eips: {} } unless component_attrs.create_nat_gateway
+
+          case component_attrs.nat_gateway_type
+          when 'single'
+            create_single_nat_gateway(name, component_attrs, public_subnets)
+          when 'per_az'
+            create_per_az_nat_gateways(name, component_attrs, public_subnets, azs)
+          else
+            { nat_gateways: {}, nat_eips: {} }
+          end
+        end
+
+        def create_single_nat_gateway(name, component_attrs, public_subnets)
+          first_public = public_subnets.values.first
+
+          eip_ref = aws_eip(resource_name(name, :nat_eip), {
+            domain: 'vpc',
+            tags: merge_component_tags(
+              component_attrs.tags,
+              { Name: "#{name}-nat-eip", Purpose: 'NAT Gateway public IP' },
+              :public_private_subnets,
+              :eip
+            )
+          })
+
+          nat_ref = aws_nat_gateway(resource_name(name, :nat_gw), {
+            allocation_id: eip_ref.id,
+            subnet_id: first_public.id,
+            tags: merge_component_tags(
+              component_attrs.tags,
+              { Name: "#{name}-nat-gw", Type: 'single', HighAvailability: 'false' },
+              :public_private_subnets,
+              :nat_gateway
+            )
+          })
+
+          { nat_gateways: { single: nat_ref }, nat_eips: { single: eip_ref } }
+        end
+
+        def create_per_az_nat_gateways(name, component_attrs, public_subnets, azs)
+          nat_gateways = {}
+          nat_eips = {}
+
+          azs.each_with_index do |az, index|
+            public_subnet = public_subnets.values[index % public_subnets.size]
+            next unless public_subnet
+
+            eip_ref = aws_eip(resource_name(name, "nat_eip_#{index + 1}"), {
+              domain: 'vpc',
+              tags: merge_component_tags(
+                component_attrs.tags,
+                { Name: "#{name}-nat-eip-#{index + 1}", AvailabilityZone: az, Purpose: 'NAT Gateway public IP' },
+                :public_private_subnets,
+                :eip
+              )
+            })
+            nat_eips[:"az_#{index + 1}"] = eip_ref
+
+            nat_ref = aws_nat_gateway(resource_name(name, "nat_gw_#{index + 1}"), {
+              allocation_id: eip_ref.id,
+              subnet_id: public_subnet.id,
+              tags: merge_component_tags(
+                component_attrs.tags,
+                { Name: "#{name}-nat-gw-#{index + 1}", AvailabilityZone: az, Type: 'per_az', HighAvailability: 'true' },
+                :public_private_subnets,
+                :nat_gateway
+              )
+            })
+            nat_gateways[:"az_#{index + 1}"] = nat_ref
+          end
+
+          { nat_gateways: nat_gateways, nat_eips: nat_eips }
+        end
+
+        def create_private_routing(name, component_attrs, vpc_id, private_subnets, nat_gateways, azs)
+          return { route_tables: {}, routes: {}, associations: {} } unless component_attrs.create_nat_gateway
+
+          case component_attrs.nat_gateway_type
+          when 'single'
+            create_single_private_routing(name, component_attrs, vpc_id, private_subnets, nat_gateways)
+          when 'per_az'
+            create_per_az_private_routing(name, component_attrs, vpc_id, private_subnets, nat_gateways, azs)
+          else
+            { route_tables: {}, routes: {}, associations: {} }
+          end
+        end
+
+        def create_single_private_routing(name, component_attrs, vpc_id, private_subnets, nat_gateways)
+          private_rt_ref = aws_route_table(resource_name(name, :private_rt), {
+            vpc_id: vpc_id,
+            tags: merge_component_tags(
+              component_attrs.tags,
+              { Name: "#{name}-private-rt", Type: 'private', Purpose: 'Private subnet routing' },
+              :public_private_subnets,
+              :route_table
+            )
+          })
+
+          private_route_ref = aws_route(resource_name(name, :private_route), {
+            route_table_id: private_rt_ref.id,
+            destination_cidr_block: '0.0.0.0/0',
+            nat_gateway_id: nat_gateways[:single].id
+          })
+
+          associations = {}
+          private_subnets.each do |subnet_key, subnet_ref|
+            assoc_name = resource_name(name, "#{subnet_key}_rt_assoc")
+            assoc_ref = aws_route_table_association(assoc_name, {
+              subnet_id: subnet_ref.id,
+              route_table_id: private_rt_ref.id
+            })
+            associations[:"#{subnet_key}_association"] = assoc_ref
+          end
+
+          { route_tables: { single: private_rt_ref }, routes: { single: private_route_ref }, associations: associations }
+        end
+
+        def create_per_az_private_routing(name, component_attrs, vpc_id, private_subnets, nat_gateways, azs)
+          route_tables = {}
+          routes = {}
+          associations = {}
+
+          azs.each_with_index do |az, index|
+            rt_ref = aws_route_table(resource_name(name, "private_rt_#{index + 1}"), {
+              vpc_id: vpc_id,
+              tags: merge_component_tags(
+                component_attrs.tags,
+                { Name: "#{name}-private-rt-#{index + 1}", Type: 'private', AvailabilityZone: az, Purpose: 'Private subnet AZ-specific routing' },
+                :public_private_subnets,
+                :route_table
+              )
+            })
+            route_tables[:"az_#{index + 1}"] = rt_ref
+
+            if nat_gateways[:"az_#{index + 1}"]
+              route_ref = aws_route(resource_name(name, "private_route_#{index + 1}"), {
+                route_table_id: rt_ref.id,
+                destination_cidr_block: '0.0.0.0/0',
+                nat_gateway_id: nat_gateways[:"az_#{index + 1}"].id
+              })
+              routes[:"az_#{index + 1}"] = route_ref
+            end
+
+            private_subnets.each_with_index do |(subnet_key, subnet_ref), subnet_index|
+              next unless subnet_index % azs.length == index
+
+              assoc_name = resource_name(name, "#{subnet_key}_rt_assoc")
+              assoc_ref = aws_route_table_association(assoc_name, {
+                subnet_id: subnet_ref.id,
+                route_table_id: rt_ref.id
+              })
+              associations[:"#{subnet_key}_association"] = assoc_ref
+            end
+          end
+
+          { route_tables: route_tables, routes: routes, associations: associations }
+        end
+      end
+    end
+  end
+end
