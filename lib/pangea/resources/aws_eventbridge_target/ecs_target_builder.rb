a67dc83b30eef493c5a2506a2fd076b1f0af3421
drzzln <drzzln@protonmail.com>
2025-12-29 21:08:16 -0500

    updates

--- lib/pangea/resources/aws_eventbridge_target/ecs_target_builder.rb
+++ lib/pangea/resources/aws_eventbridge_target/ecs_target_builder.rb
@@ -19,38 +19,44 @@ module Pangea
     module AWS
       # Builder module for ECS target parameters in EventBridge targets
       module EcsTargetBuilder
-        # Builds ECS parameters block for EventBridge target
-        # @param builder [Object] The DSL builder context
+        module_function
+
+        # Returns a proc that builds ECS parameters in DSL context
         # @param ecs_params [Hash] ECS parameters configuration
-        def build_ecs_parameters(builder, ecs_params)
-          builder.ecs_parameters do
+        # @return [Proc] Block to be instance_exec'd in DSL context
+        def ecs_parameters_block(ecs_params)
+          proc do
             task_definition_arn ecs_params[:task_definition_arn]
             task_count ecs_params[:task_count] if ecs_params[:task_count]
             launch_type ecs_params[:launch_type] if ecs_params[:launch_type]
             platform_version ecs_params[:platform_version] if ecs_params[:platform_version]
             group ecs_params[:group] if ecs_params[:group]
 
-            build_network_configuration(self, ecs_params[:network_configuration]) if ecs_params[:network_configuration]
-            build_capacity_provider_strategy(self, ecs_params[:capacity_provider_strategy]) if ecs_params[:capacity_provider_strategy]
-            build_placement_constraints(self, ecs_params[:placement_constraint]) if ecs_params[:placement_constraint]
-            build_placement_strategy(self, ecs_params[:placement_strategy]) if ecs_params[:placement_strategy]
-            build_ecs_tags(self, ecs_params[:tags]) if ecs_params[:tags]
+            if ecs_params[:network_configuration]
+              instance_exec(ecs_params[:network_configuration], &EcsTargetBuilder.network_config_block)
+            end
+            EcsTargetBuilder.build_capacity_strategies(self, ecs_params[:capacity_provider_strategy])
+            EcsTargetBuilder.build_placement_constraints(self, ecs_params[:placement_constraint])
+            EcsTargetBuilder.build_placement_strategies(self, ecs_params[:placement_strategy])
+            EcsTargetBuilder.build_tags(self, ecs_params[:tags])
           end
         end
 
-        private
-
-        def build_network_configuration(builder, network_config)
-          builder.network_configuration do
-            awsvpc_configuration do
-              subnets network_config[:awsvpc_configuration][:subnets]
-              security_groups network_config[:awsvpc_configuration][:security_groups] if network_config[:awsvpc_configuration][:security_groups]
-              assign_public_ip network_config[:awsvpc_configuration][:assign_public_ip] if network_config[:awsvpc_configuration][:assign_public_ip]
+        def network_config_block
+          proc do |network_config|
+            network_configuration do
+              awsvpc_configuration do
+                subnets network_config[:awsvpc_configuration][:subnets]
+                security_groups network_config[:awsvpc_configuration][:security_groups] if network_config[:awsvpc_configuration][:security_groups]
+                assign_public_ip network_config[:awsvpc_configuration][:assign_public_ip] if network_config[:awsvpc_configuration][:assign_public_ip]
+              end
             end
           end
         end
 
-        def build_capacity_provider_strategy(builder, strategies)
+        def build_capacity_strategies(builder, strategies)
+          return unless strategies
+
           strategies.each do |strategy|
             builder.capacity_provider_strategy do
               capacity_provider strategy[:capacity_provider]
@@ -61,6 +67,8 @@ module Pangea
         end
 
         def build_placement_constraints(builder, constraints)
+          return unless constraints
+
           constraints.each do |constraint|
             builder.placement_constraint do
               type constraint[:type] if constraint[:type]
@@ -69,7 +77,9 @@ module Pangea
           end
         end
 
-        def build_placement_strategy(builder, strategies)
+        def build_placement_strategies(builder, strategies)
+          return unless strategies
+
           strategies.each do |strategy|
             builder.placement_strategy do
               type strategy[:type] if strategy[:type]
@@ -78,11 +88,11 @@ module Pangea
           end
         end
 
-        def build_ecs_tags(builder, ecs_tags)
+        def build_tags(builder, ecs_tags)
+          return unless ecs_tags
+
           builder.tags do
-            ecs_tags.each do |key, value|
-              public_send(key, value)
-            end
+            ecs_tags.each { |key, value| public_send(key, value) }
           end
         end
       end
